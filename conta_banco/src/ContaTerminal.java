51503550eaa44c34ea4b0cdb05810fe453a633c4
Lucas freitas <lucasft00@gmail.com>
2026-03-24 09:49:55 -0300

    Implementar lógica da conta terminal bancária
    - Adiciona entrada de dados do usuário (número da conta, agência, nome, saldo)
    - Exibe confirmação com os dados da conta criada

--- conta_banco/src/ContaTerminal.java
+++ conta_banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;   
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in);
+        
+    System.out.print("Digite o número da conta: ");
+        int Numero = scanner.nextInt();
+        
+    
+    System.out.print("Digite o número da agência: ");
+        String agencia = scanner.next();
+        scanner.nextLine();
+    
+    System.out.print("Digite o Seu nome : ");
+        String nome = scanner.nextLine();
+    
+    System.out.print("Digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+        
+    
+    System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + Numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+
     }
 }
